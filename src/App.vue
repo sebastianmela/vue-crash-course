8f9d9f16b2f27a490b372c69600898b8b49731be
Sebita <sebamelamieres5@hotmail.com>
2025-02-16 20:55:06 -0300

    added routing

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,9 @@
 <script setup>
 import Navbar from "@/components/Navbar.vue";
-import Hero from "@/components/Hero.vue";
-import HomeCards from "@/components/HomeCards.vue";
-import JobsListings from "@/components/JobListings.vue";
+import { RouterView } from "vue-router";
 </script>
+
 <template>
 	<Navbar />
-	<Hero title="Test"/>
-	<HomeCards />
-  <JobsListings :limit="3" :showButton="true"/>
+	<RouterView />
 </template>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,11 @@
+<script setup>
+import Hero from "@/components/Hero.vue";
+import HomeCards from "@/components/HomeCards.vue";
+import JobsListings from "@/components/JobListings.vue";
+</script>
+
+<template>
+	<Hero title="Test" />
+	<HomeCards />
+	<JobsListings :limit="3" :showButton="true" />
+</template>
