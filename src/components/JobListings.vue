a347ff7525fe2c1980cb840f7df3835eee85061c
Sebita <sebamelamieres5@hotmail.com>
2025-02-16 21:50:25 -0300

    added spinner

--- src/components/JobListings.vue
+++ src/components/JobListings.vue
@@ -1,8 +1,9 @@
 <script setup>
 import { RouterLink } from "vue-router";
 import JobListing from "@/components/JobListing.vue";
-import { ref, defineProps, onMounted, reactive } from "vue";
+import { defineProps, onMounted, reactive } from "vue";
 import axios from "axios";
+import PulseLoader from "vue-spinner/src/PulseLoader.vue";
 
 defineProps({
 	limit: Number,
@@ -35,7 +36,10 @@ onMounted(async () => {
 			<h2 class="text-3xl font-bold text-green-500 mb-6 text-center">
 				Browse Jobs
 			</h2>
-			<div class="grid grid-cols-1 md:grid-cols-3 gap-6">
+			<div v-if="state.isLoading" class="text-center text-green-500 py-6">
+				<PulseLoader />
+			</div>
+			<div v-else class="grid grid-cols-1 md:grid-cols-3 gap-6">
 				<JobListing
 					v-for="job in state.jobs.slice(0, limit || state.jobs.length)"
 					:key="job.id"
