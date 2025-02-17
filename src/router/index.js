5168cd29d27cbc1615bb8a3d467da3a762de3050
Sebita <sebamelamieres5@hotmail.com>
2025-02-16 21:27:44 -0300

    created JobView

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,8 @@ import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "@/views/HomeView.vue";
 import JobsView from "@/views/JobsView.vue";
 import NotFoundView from "@/views/NotFoundView.vue";
+import JobView from "@/views/JobView.vue";
+
 const router = createRouter({
 	history: createWebHistory(import.meta.env.BASE_URL),
 	routes: [
@@ -10,16 +12,21 @@ const router = createRouter({
 			name: "home",
 			component: HomeView,
 		},
-        {
+		{
 			path: "/jobs",
 			name: "jobs",
 			component: JobsView,
 		},
 		{
-			path: '/:catchAll(.*)',
-			name: 'not-found',
+			path: "/jobs/:id",
+			name: "job",
+			component: JobView,
+		},
+		{
+			path: "/:catchAll(.*)",
+			name: "not-found",
 			component: NotFoundView,
-		}
+		},
 	],
 });
 
